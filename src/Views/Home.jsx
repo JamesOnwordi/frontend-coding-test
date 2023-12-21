d4929b2803f1086f59134d6348f991435b4cb76f
unknown <jamesonwordi50@gmail.com>
2023-12-21 06:11:49 -0700

    ready for submission

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -5,12 +5,12 @@ import Navbar from "../Components/Navbar/Navbar";
 function Home() {
   const [communities, setCommunities] = useState([]);
   const [homes, setHomes] = useState([]);
+  // to keep track of both communities' and homes' axios call.
   const updatesComplete = useRef({ communities: false, homes: false });
   const [all, setAll] = useState([]);
   const [toshow, setToShow] = useState([]);
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState([]);
   const [info, setInfo] = useState([]);
-  const [map, setMap] = useState([]);
   const strings = [
     "Welcome to Calgary! Home to bustling urban areas and quiet rural neigborhoods alike, the city is divided into more than 184 communities. The Calgary community map is further broken down into quadrants (Northwest, Northeast, Southwest, and Southeast), with eight zones that you can search in. Feel free to explore the beautiful residential communities and park the city has to offer.",
   ];
@@ -22,8 +22,15 @@ function Home() {
         setCommunities(response.data);
         updatesComplete.current.communities = true;
       })
+      // for handling error with api call
       .catch((error) => {
         console.error("Error fetching data", error);
+        if (error.response) {
+          console.error("Server error", error.response.data);
+          console.error("Status code:", error.response.status);
+        } else if (error.request)
+          console.error("No response from the server:", error.request);
+        else console.error("Error setting up request:", error.message);
       });
     axios
       .get("/api/homes.json")
@@ -33,52 +40,58 @@ function Home() {
       })
       .catch((error) => {
         console.error("Error fetching data", error);
+        if (error.response) {
+          console.error("Server error", error.response.data);
+          console.error("Status code:", error.response.status);
+        } else if (error.request)
+          console.error("No response from the server:", error.request);
+        else console.error("Error setting up request:", error.message);
       });
   }, []);
 
   useEffect(() => {
     if (updatesComplete.current.communities && updatesComplete.current.homes) {
-      console.log(communities,homes)
-      if(communities){
+      if (communities) {
         const val = communities
-        .map((community, ind) => {
-          const presentHome = homes.find(
-            (element) => element.communityId === community.id
-          );
+          .map((community, ind) => {
+            const presentHome = homes.find(
+              (element) => element.communityId === community.id
+            );
 
-          // adds extra pictures to the database
-          if (community.name == "Oakridge") community.imgUrl = "assets/oakridge.jpeg";
-          if (community.name == "Rosedale") community.imgUrl = "assets/rosedale.jpg";
+            // adds extra pictures to the database
+            if (community.name == "Oakridge")
+              community.imgUrl = "assets/oakridge.jpeg";
+            if (community.name == "Rosedale")
+              community.imgUrl = "assets/rosedale.jpg";
+            if (community.name == "AuburnBay") community.group = "South East";
 
-          if (presentHome)
-            return {
-              name: community.name,
-              image: community.imgUrl,
-              type: presentHome.type,
-              price: presentHome.price,
-              group: community.group,
-            };
-        })
-        .sort((a, b) => {
-          return a.name[0].localeCompare(b.name[0]);
-        });
+            if (presentHome)
+              return {
+                name: community.name,
+                image: community.imgUrl,
+                type: presentHome.type,
+                price: presentHome.price,
+                group: community.group,
+              };
+          })
+          .sort((a, b) => {
+            return a.name[0].localeCompare(b.name[0]);
+          });
         setAll(val);
         display(val);
       }
-      
-
-      
     }
   }, [communities, homes]);
 
   useEffect(() => {
     display(
       all.filter((selected) => {
-        if (search)
+        if (search.length)
           if (selected)
             return selected.name.toLowerCase().includes(search.toLowerCase());
       })
     );
+    if (!search) display(all);
   }, [search]);
 
   const display = (all) => {
@@ -86,9 +99,9 @@ function Home() {
       all.map((value, ind) => {
         if (value)
           return (
-            <div key={value+ind}className="group">
+            <div key={value + ind}>
               <div className="sm:w-80 rounded bg-white border-2 border-teal-900 transition ease-in-out delay-100 hover:shadow-2xl  hover:scale-105 shadow-lg m-4 sm:m-8">
-                <div className="sm:m-6 h-56  flex justify-center items-center overflow-hidden  h-fill duration-200 ">
+                <div className="sm:m-6 h-56 flex justify-center items-center overflow-hidden  h-fill duration-200 ">
                   <img
                     className="transition w-full h-56 object-cover ease-in-out delay-150  h-fill  duration-200 rounded "
                     src={value.image}
@@ -101,12 +114,12 @@ function Home() {
                     </p>
                   </div>
                   <div className="text-teal-600 italic">
-                    This is the community of {value.name}, it is located in the{" "}
+                    The community of {value.name}, is located in the
                     {value.area} at the {value.group} part of Calgary with
-                    majority of the buildings being {value.type}
+                    majority of it's buildings being {value.type}.
                   </div>
                   <div className="text-base ">
-                    The average price of homes in {value.name} is:"$
+                    The average price of homes in {value.name} is: $
                     {value.price.toLocaleString()}
                   </div>
                 </div>
@@ -124,21 +137,19 @@ function Home() {
         setSearch={setSearch}
         info={info}
         setInfo={setInfo}
-        map={map}
-        setMap={setMap}
       />
 
       <div className="m-h-96 overflow:hidden">
         {!info && (
           <div>
-            <div className="sm:ml-36 sm:mr-36 sm:ml-12 sm:mr-12 border-2 border-black p-4">
-                {strings}
+            <div className="sm:ml-36 sm:mr-36 ml-6 mr-6 border-2 border-black p-4">
+              {strings}
             </div>
           </div>
         )}
       </div>
 
-      <div className="flex justify-center mt- flex-wrap">{toshow}</div>
+      <div className="flex justify-center flex-wrap">{toshow}</div>
     </>
   );
 }
